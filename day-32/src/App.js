17b0133e2a43eb1dfc107cb32ccb5211a77c24e1
Mary Wells <m.a.wells@outlook.com>
2021-12-08 17:03:44 +0000

    feat(day-33)

--- day-32/src/App.js
+++ day-32/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
+import { useSpring, animated } from '@react-spring/three';
 
-function Box() {
+function Sphere() {
+  const [active, setActive] = useState(true);
+  const { color } = useSpring({
+    config: { duration: 900 },
+    color: active ? '#caa316' : '#5c4a0a',
+  });
   return (
-    <mesh>
-      <boxBufferGeometry attach='geometry' />
-      <meshStandardMaterial attach='material' color='lime' />
-    </mesh>
+    <animated.mesh scale={1.5} onPointerOver={() => setActive(!active)}>
+      <sphereBufferGeometry attach='geometry' />
+      <animated.meshStandardMaterial attach='material' color={color} />
+    </animated.mesh>
   );
 }
 
@@ -15,10 +21,10 @@ export default function App() {
   return (
     <Canvas>
       <OrbitControls />
-      <Stars />
+      <Stars radius={200} />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Box />
+      <Sphere />
     </Canvas>
   );
 }
